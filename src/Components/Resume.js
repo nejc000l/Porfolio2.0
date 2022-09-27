c7d057691dfe2b847702b07b4b318bd13ed8ef2f
nejc000l <nejc.lampic@gmail.com>
2022-09-27 15:34:25 +0200

    commite

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,18 +3,7 @@ import React from "react";
 const Resume = ({ data }) => {
   if (data) {
     var skillmessage = data.skillmessage;
-    var education = data.education.map(function (education) {
-      return (
-        <div key={education.school}>
-          <h3>{education.school}</h3>
-          <p className="info">
-            {education.degree} <span>&bull;</span>
-            <em className="date">{education.graduated}</em>
-          </p>
-          <p>{education.description}</p>
-        </div>
-      );
-    });
+
  
     var skills = data.skills.map(function (skills) {
       var className = "bar-expand " + skills.name.toLowerCase();
@@ -31,14 +20,12 @@ const Resume = ({ data }) => {
     <section id="resume">
       <div className="row education">
         <div className="three columns header-col">
-          <h1>
-            <span>Education</span>
-          </h1>
+
         </div>
 
         <div className="nine columns main-col">
           <div className="row item">
-            <div className="twelve columns">{education}</div>
+            <div className="twelve columns"></div>
           </div>
         </div>
       </div>
